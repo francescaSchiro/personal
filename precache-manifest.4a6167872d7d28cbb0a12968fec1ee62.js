9f4e0814f55b65e1a38dc789c8b032da0f23540b
Schiro Francesca <francesca.schiro@sisal.it>
2019-01-11 18:04:56 +0100

    Updates

--- precache-manifest.4a6167872d7d28cbb0a12968fec1ee62.js
+++ precache-manifest.4a6167872d7d28cbb0a12968fec1ee62.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/personal/static/js/runtime~main.bd80bb5a.js"
   },
   {
-    "revision": "c32bb5be84d4e7eb1d2f",
-    "url": "/personal/static/js/main.c32bb5be.chunk.js"
+    "revision": "1122e2b98515832e4517",
+    "url": "/personal/static/js/main.1122e2b9.chunk.js"
   },
   {
     "revision": "ddb31e98d65318384f30",
     "url": "/personal/static/js/1.ddb31e98.chunk.js"
   },
   {
-    "revision": "c32bb5be84d4e7eb1d2f",
+    "revision": "1122e2b98515832e4517",
     "url": "/personal/static/css/main.bf873da5.chunk.css"
   },
   {
-    "revision": "02d02719114b7fa145c43165c2826da2",
+    "revision": "fde82439edb1715fa8c1cde911627a5f",
     "url": "/personal/index.html"
   }
 ];
