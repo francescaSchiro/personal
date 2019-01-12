628f3ac54ba2eaa9cf90112715744cd746da3d92
francesca schiro <francesca_schiro@yahoo.it>
2019-01-12 01:30:35 +0100

    Updates

--- precache-manifest.d995d29ec501df022725abbd703d92dc.js
+++ precache-manifest.d995d29ec501df022725abbd703d92dc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/personal/static/js/runtime~main.bd80bb5a.js"
   },
   {
-    "revision": "619c4801673c87f66d1d",
-    "url": "/personal/static/js/main.619c4801.chunk.js"
+    "revision": "79c7ab658b093c81720a",
+    "url": "/personal/static/js/main.79c7ab65.chunk.js"
   },
   {
     "revision": "ddb31e98d65318384f30",
     "url": "/personal/static/js/1.ddb31e98.chunk.js"
   },
   {
-    "revision": "619c4801673c87f66d1d",
+    "revision": "79c7ab658b093c81720a",
     "url": "/personal/static/css/main.2741b068.chunk.css"
   },
   {
-    "revision": "2deef198e098b672d8affd5d9e1a021e",
+    "revision": "84334e6e3853617e02d16a477a06f190",
     "url": "/personal/index.html"
   }
 ];
