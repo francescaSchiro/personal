94ca8cd3cf457de70e59e8bbee5e47e2a492e0c9
francesca schiro <francesca_schiro@yahoo.it>
2019-01-12 02:31:08 +0100

    FIX anchor working wihout hover and without scrolling just jumping

--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -8,28 +8,29 @@ const NavItem = styled.a`
   margin-right: 4px;
   position: relative;
   /* border: 2px solid orange; */
-  font-size: ${props => props.theme.font.size.xl};
+  font-size: ${props => props.theme.font.size.l};
   
-  /* &:hover {
-    font-weight: ${props => props.theme.font.weight.bold};
-    &:before {
-      content: "";
-      position: absolute;
-      top: 5px;
-      width: 100%;
-      height: 80%;
-      background: ${props => props.sectionColor};
-      z-index: -1;
-    }
-   } */
-  @media screen and (min-width: ${getBreakpoint(
+  /* @media screen and (min-width: ${getBreakpoint(
     "tablet"
   )}) and (max-width: ${getBreakpoint("desktop")}) {
-    /* regole CSS */
-    }
-  @media screen and (max-width: ${getBreakpoint("tablet")}) {
-    /* regole CSS */
-    font-size: ${props => props.theme.font.size.l};
+    } */
+
+    /* PUT BACK OVER when not touch*/
+    @media screen and (min-width: ${getBreakpoint("tablet")}) {
+      /* regole CSS */
+      font-size: ${props => props.theme.font.size.xl};
+                &:hover {
+                  font-weight: ${props => props.theme.font.weight.bold};
+                  &:before {
+                    content: "";
+                    position: absolute;
+                    top: 5px;
+                    width: 100%;
+                    height: 80%;
+                    background: ${props => props.sectionColor};
+                    z-index: -1;
+                  }
+                 }
   }
 `;
 
