0118e920311d8e267ba87128aaed0720d5b7fa1c
Schiro Francesca <francesca.schiro@sisal.it>
2019-01-14 15:10:13 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal/precache-manifest.f35d062962d2904f77549a6c93f5805a.js"
+  "/personal/precache-manifest.a9e3550fb2bd82d395edd2453ee6fcf6.js"
 );
 
 workbox.clientsClaim();
