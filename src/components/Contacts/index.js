457e7fa00baf95618e47ffc9c6f293c064e2262e
Schiro Francesca <francesca.schiro@sisal.it>
2019-01-14 15:48:42 +0100

    commit before deploy

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+
+import { checkIfAppleDevice } from '../../utils';
+
 import Wrapper from './Wrapper';
 import SectionTitle from '../SectionTitle';
 import SectionTitleContainer from './SectionTitleContainer';
@@ -12,49 +15,68 @@ import H3 from '../H3';
 const Contacts = () => (
   <Wrapper>
     <SectionTitleContainer className='contactsSection' id='contacts'>
-      <Fade left>
+      {checkIfAppleDevice ? (
         <SectionTitle
           margins={'90px -10px 0 0'}
           sectionColor={props => props.theme.color.lightBlue}
         >
           Contacts
         </SectionTitle>
-      </Fade>
+      ) : (
+        <Fade left>
+          <SectionTitle
+            margins={'90px -10px 0 0'}
+            sectionColor={props => props.theme.color.lightBlue}
+          >
+            Contacts
+          </SectionTitle>
+        </Fade>
+      )}
     </SectionTitleContainer>
-    <Fade right>
+  
       <BodyWrapper>
         <LinksWrapper>
-          <H3>
-          You can find me on:
-          </H3>
-          <div style={{marginTop: '20px'}}>
-            <i className="fab fa-github-square"></i>
-              <LinkContacts 
-                href='https://github.com/francescaSchiro'
-                target='_blank'
-                rel='noopener noreferrer' 
-                sectionColor={props=> props.theme.color.lightBlue} > Git Hub </LinkContacts>
+          <H3>You can find me on:</H3>
+          <div style={{ marginTop: '20px' }}>
+            <i className='fab fa-github-square' />
+            <LinkContacts
+              href='https://github.com/francescaSchiro'
+              target='_blank'
+              rel='noopener noreferrer'
+              sectionColor={props => props.theme.color.lightBlue}
+            >
+              {' '}
+              Git Hub{' '}
+            </LinkContacts>
           </div>
-          <div style={{marginTop: '20px'}}>
-            <i className="fab fa-behance-square"></i>
-              <LinkContacts
-                href='https://www.behance.net/francescaschiro'
-                target='_blank'
-                rel='noopener noreferrer'
-                sectionColor={props=> props.theme.color.lightBlue}> Behance </LinkContacts>
+          <div style={{ marginTop: '20px' }}>
+            <i className='fab fa-behance-square' />
+            <LinkContacts
+              href='https://www.behance.net/francescaschiro'
+              target='_blank'
+              rel='noopener noreferrer'
+              sectionColor={props => props.theme.color.lightBlue}
+            >
+              {' '}
+              Behance{' '}
+            </LinkContacts>
           </div>
-          <div style={{marginTop: '20px'}}>
-            <i className="fab fa-linkedin"></i>
-              <LinkContacts
-                href='www.linkedin.com/in/francesca-schiro'
-                target='_blank'
-                rel='noopener noreferrer'
-                sectionColor={props=> props.theme.color.lightBlue}> Linked In </LinkContacts>
+          <div style={{ marginTop: '20px' }}>
+            <i className='fab fa-linkedin' />
+            <LinkContacts
+              href='www.linkedin.com/in/francesca-schiro'
+              target='_blank'
+              rel='noopener noreferrer'
+              sectionColor={props => props.theme.color.lightBlue}
+            >
+              {' '}
+              Linked In{' '}
+            </LinkContacts>
           </div>
-          <MailFlipForm>Mail me </MailFlipForm> 
+          <MailFlipForm>Mail me </MailFlipForm>
         </LinksWrapper>
       </BodyWrapper>
-    </Fade>
+  
   </Wrapper>
 );
 
--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -1,22 +1,32 @@
-import React from "react";
-import Zoom from "react-reveal/Zoom";
-import Wrapper from "./Wrapper";
-import SectionTitle from "../SectionTitle";
-import SectionTitleContainer from "./SectionTitleContainer";
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+import Wrapper from './Wrapper';
+import SectionTitle from '../SectionTitle';
+import SectionTitleContainer from './SectionTitleContainer';
 // import CarouselSlider from './CarouselSlider';
-import CarouselHandmade from "./CarouselHandmade";
+import CarouselHandmade from './CarouselHandmade';
+import { checkIfAppleDevice } from '../../utils';
 
 const Works = () => (
   <Wrapper>
-    <SectionTitleContainer className="worksSection" id="works">
-      <Zoom>
+    <SectionTitleContainer className='worksSection' id='works'>
+      {checkIfAppleDevice ? (
         <SectionTitle
-          margins={"90px 0 0 0"}
+          margins={'90px 0 0 0'}
           sectionColor={props => props.theme.color.red}
         >
           Works
         </SectionTitle>
-      </Zoom>
+      ) : (
+        <Zoom>
+          <SectionTitle
+            margins={'90px 0 0 0'}
+            sectionColor={props => props.theme.color.red}
+          >
+            Works
+          </SectionTitle>
+        </Zoom>
+      )}
     </SectionTitleContainer>
     <CarouselHandmade />
   </Wrapper>
