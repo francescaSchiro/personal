d5dda422813ab5836e5273b5fed7878d615895cf
francesca schiro <francesca_schiro@yahoo.it>
2019-01-17 09:24:41 +0100

    aboutme change

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Fade from 'react-reveal/Fade';
-import Wrapper from './Wrapper';
-import SectionTitleContainer from './SectionTitleContainer';
-import PContainer from './PContainer';
-import SectionTitle from '../SectionTitle';
-import P from '../P';
-import LinkContainer from './LinkContainer';
-import Link from '../Link';
-import { checkIfAppleDevice, isIE } from '../../utils';
+import React from "react";
+import Fade from "react-reveal/Fade";
+import Wrapper from "./Wrapper";
+import SectionTitleContainer from "./SectionTitleContainer";
+import PContainer from "./PContainer";
+import SectionTitle from "../SectionTitle";
+import P from "../P";
+import LinkContainer from "./LinkContainer";
+import Link from "../Link";
+import { checkIfAppleDevice, isIE } from "../../utils";
 
 // { checkIfAppleDevice && <div>asfkgfkwqfufgqiwu</div> }
 
 const About = () => (
   <Wrapper>
-    {(checkIfAppleDevice || isIE) ? (
+    {checkIfAppleDevice || isIE ? (
       // if apple device
       <React.Fragment>
-        <SectionTitleContainer className='aboutSection' id='about'>
+        <SectionTitleContainer className="aboutSection" id="about">
           <SectionTitle
-            margins={'90px 0 0 -20px'}
+            margins={"90px 0 0 -20px"}
             sectionColor={props => props.theme.color.yellow}
           >
             About
@@ -27,20 +27,28 @@ const About = () => (
 
         <PContainer>
           <P sectionColor={props => props.theme.color.yellow}>
-            Ciao. 
-            My Name is Francesca, I'm a graphic designer & front end developer based in the town of Bergamo. 
-            I love discovering new things. I believe that “the less you do, the less you’d do” and this is why I am always active, pushed by a constant curiosity. 
-            My experience and education enriched my graphic knowledge, for web and print, and increased my awareness of it as a powerful mean of communication, essential within a very competitive digital reality.
-            After a Master in coding I embraced a career in Front End Development.
+            Ciao. <br />
+            I am Francesca. <br />
+            I think pufferfish are pretty cool. <br />
+            Discovering new places and meeting new people <br />
+            makes me feel alive. <br />
+            I like sports and getting my outdoor fix once in a while. <br />
+            I give 100% in everything I do. <br />
+            Knowing that there's always something to learn makes me happy.{" "}
+            <br />
           </P>
+          <span style={{ fontWeight: "bold" }}>
+            ... And I'm a Graphic Designer & Front End Developer living in
+            Bergamo.{" "}
+          </span>
         </PContainer>
 
         <LinkContainer>
           <Link
             sectionColor={props => props.theme.color.yellow}
-            href='https://docdro.id/U6bDALF'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://docdro.id/U6bDALF"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Curious? Take a look at my CV
           </Link>
@@ -50,10 +58,10 @@ const About = () => (
       // if NOT apple device: import Fade animations from reactreval
 
       <React.Fragment>
-        <SectionTitleContainer className='aboutSection' id='about'>
+        <SectionTitleContainer className="aboutSection" id="about">
           <Fade left>
             <SectionTitle
-              margins={'90px 0 0 -20px'}
+              margins={"90px 0 0 -20px"}
               sectionColor={props => props.theme.color.yellow}
             >
               About
@@ -64,11 +72,14 @@ const About = () => (
         <Fade right>
           <PContainer>
             <P sectionColor={props => props.theme.color.yellow}>
-            Ciao. 
-            My Name is Francesca, I'm a graphic designer & front end developer based in the town of Bergamo. 
-            I love discovering new things. I believe that “the less you do, the less you’d do” and this is why I am always active, pushed by a constant curiosity. 
-            My experience and education enriched my graphic knowledge, for web and print, and increased my awareness of it as a powerful mean of communication, essential within a very competitive digital reality.
-            After a Master in coding I embraced a career in Front End Development.
+              Ciao. <br />
+              I am Francesca, a Graphic Designer & Front End Developer living in
+              Bergamo. <br />
+              I think pufferfish are pretty cool. <br />
+              Discovering new places and meeting new people makes me feel alive. <br />
+              I like sports and getting my outdoor fix once in a while. <br />
+              I give 100% in everything I do. <br />
+              Knowing that there's always something to learn makes me happy. <br />
             </P>
           </PContainer>
         </Fade>
@@ -76,9 +87,9 @@ const About = () => (
           <Fade>
             <Link
               sectionColor={props => props.theme.color.yellow}
-              href='https://docdro.id/U6bDALF'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://docdro.id/U6bDALF"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Curious? Take a look at my CV
             </Link>
@@ -90,3 +101,9 @@ const About = () => (
 );
 
 export default About;
+
+// Ciao.
+//             My Name is Francesca, I'm a graphic designer & front end developer based in the town of Bergamo.
+//             I love discovering new things. I believe that “the less you do, the less you’d do” and this is why I am always active, pushed by a constant curiosity.
+//             My experience and education enriched my graphic knowledge, for web and print, and increased my awareness of it as a powerful mean of communication, essential within a very competitive digital reality.
+//             After a Master in coding I embraced a career in Front End Development.
